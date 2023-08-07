309c19231268fc0b3179244fd279ab5ca96bc66d
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-08-06 20:36:59 -0500

    Gun Controller basic

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Gun Aspects")]
+    // Add all invidual aspects in editor
+    public float damage;
+    public float range;
+    public float fireRate;
+    public float impactForce;
+
+    private float nextTimeToFire = 0f;
+
+    [Header("Fire Points and Particle Effects")]
+    [SerializeField] Transform firePoint;
+    // Particle effects later
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.rigidbody != null)
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            
+        }
     }
 }
